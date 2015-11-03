7c372a61e2efa1bee542293a647ee11a1f4207a9
Jeff Buttars <jeffbuttars@gmail.com>
2015-11-03 08:48:32 -0700

    Register remote plugins after install/update. Also, build the latest tagged version instead of master if possible.

--- _upkg/install_nvim.sh
+++ _upkg/install_nvim.sh
@@ -6,15 +6,33 @@ GIT_REPO='https://github.com/neovim/neovim.git'
 
 sudo mkdir -p "$NEOVIM_DST_DIR"
 
+
 if [[ -d "$GIT_DST_DIR" ]]; then
     cd "$GIT_DST_DIR"
-    git pull
+    git fetch
     cd -
 else
     git clone "$GIT_REPO" "$GIT_DST_DIR"
 fi
 
 cd "$GIT_DST_DIR"
+
+# Use the most recent version tag to get latest 'stable'
+vtag=$(git tag | grep '^v.*' | sort -r | head -n 1)
+if [[ -z $vtag ]]; then
+    vtag="master"
+fi
+
+pr_info "Using Nvim branch/tag $vtag"
+sleep 1
+
+git checkout "$vtag"
+git merge
+
 make -j8 CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX:PATH=${NEOVIM_DST_DIR}"
 sudo make install
 cd -
+
+pr_info "Updating Remote Plugins..."
+nvim -c "UpdateRemotePlugins" -c "qa"
+wait
