e75e13fd8cbb25cf2516c0b0500ab7b775a33084
Jeff Buttars <jeffbuttars@gmail.com>
2021-04-10 11:09:45 -0600

    wip

--- _upkg/vim_plug.sh
+++ _upkg/vim_plug.sh
@@ -1,3 +1,8 @@
 pr_info "Installing Vim Plug."
 curl -fLo "$NVIM_HOME/autoload/plug.vim" --create-dirs \
 https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+pr_info "Installing Paq..."
+mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/pack/paqs/opt/paq-nvim"
+git clone https://github.com/savq/paq-nvim.git \
+    "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/pack/paqs/opt/paq-nvim"
---
+++ nvim/lua/local/paq_ages.lua
@@ -0,0 +1,99 @@
+-- https://github.com/savq/paq-nvim
+-- More or less 'borrowed' straight from the repo readme.
+
+-- Load Paq
+vim.cmd 'packadd paq-nvim'         -- Load package
+local paq = require'paq-nvim'.paq  -- Import module and bind `paq` function
+paq{'savq/paq-nvim', opt=true}     -- Let Paq manage itself
+
+
+-- For writing prose
+paq 'junegunn/goyo.vim'
+
+-- Python
+paq 'psf/black'
+paq {'stsewd/isort.nvim',  run=vim.fn['remote#host#UpdateRemotePlugins'] }
+
+
+-- FZF setup
+paq {'junegunn/fzf', run=vim.fn['fzf#install'] }
+paq 'junegunn/fzf.vim'
+
+
+paq 'https://github.com/vim-airline/vim-airline-themes'
+paq 'https://github.com/vim-airline/vim-airline.git'
+
+-- Iconogrophy
+paq 'junegunn/vim-emoji'
+paq 'ryanoasis/vim-devicons'
+
+
+-- Nerd comments and tree explorer
+paq 'preservim/nerdcommenter'
+paq 'https://github.com/Xuyuanp/nerdtree-git-plugin'
+paq 'vwxyutarooo/nerdtree-devicons-syntax'
+
+-- Vim Tree
+paq 'kyazdani42/nvim-web-devicons' " for file icons
+paq 'kyazdani42/nvim-tree.lua'
+
+
+-- Unixy commands for vim
+paq 'https://github.com/tpope/vim-eunuch.git'
+
+paq {'numirias/semshi', run=vim.fn['remote#host#UpdateRemotePlugins']}
+
+paq 'axelf4/vim-strip-trailing-whitespace'
+
+-- 
+-- " One day, I shall resurrect this trap
+-- paq {'jeffbuttars/neovim-doubletap', run=vim.fn['remote#host#UpdateRemotePlugins']}
+
+-- Lua utils / lib
+paq 'nvim-lua/plenary.nvim'
+
+-- Native LSP
+paq 'neovim/nvim-lspconfig'
+
+
+-- LSP Completion stuff
+--
+paq 'nvim-lua/completion-nvim'
+
+-- paq 'hrsh7th/nvim-compe'
+paq {'tzachar/compe-tabnine', run='./install.sh' }
+paq 'tamago324/compe-zsh'
+paq 'onsails/lspkind-nvim'
+
+paq {'nvim-treesitter/nvim-treesitter', run=vim.fn['TSUpdate']}
+-- paq 'nvim-treesitter/completion-treesitter'
+
+paq 'kyazdani42/nvim-web-devicons'
+
+
+paq 'PratikBhusal/vim-grip'
+
+-- " Git
+--
+paq 'https://github.com/tpope/vim-fugitive.git'
+paq 'lewis6991/gitsigns.nvim'
+
+paq 'rbong/vim-flog'
+paq 'ajorgensen/vim-markdown-toc'
+
+
+-- Load Ultisnips later to make sure it has the <tab> map
+paq 'SirVer/ultisnips'
+paq 'honza/vim-snippets'
+
+-- Color Schemes
+-- Solarized has been good to me. I plan to keep it a while
+paq 'iCyMind/NeoSolarized'
+--
+-- paq 'morhetz/gruvbox'
+-- 
+-- " https://github.com/lifepillar/vim-gruvbox8
+-- paq 'lifepillar/vim-gruvbox8'
+-- 
+-- " https://github.com/lifepillar/vim-solarized8
+-- " paq 'lifepillar/vim-solarized8'
