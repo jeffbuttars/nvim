6dd5cdc0bc80fbd573531e53b383c368529e139b
Jeff Buttars <jeffbuttars@gmail.com>
2021-10-14 14:12:33 -0600

    Telescope tweaks

--- nvim/lua/local/init.lua
+++ nvim/lua/local/init.lua
@@ -1,19 +1,20 @@
 
--- require("local/lspkind");
 require("local/lsp");
 require("local/pylsp");
--- require("local/pyright");
 require("local/treesitter");
 require("local/web-devicons");
 require("local/gitsigns");
 require("local/diffview");
 require("local/dirdiff");
--- require("local/lsp-trouble");
 require("local/nvim-cmp");
 require("local/nvim_tree");
 require("local/lsp_colors");
--- require("local/flow");
 require("local/tsserver");
 require("local/telescope");
 require("local/nvim-solarized");
 require("local/lualine");
+
+-- require("local/flow");
+-- require("local/lsp-trouble");
+-- require("local/pyright");
+-- require("local/lspkind");
--- nvim/lua/local/telescope.lua
+++ nvim/lua/local/telescope.lua
@@ -59,12 +59,20 @@ require('telescope').setup{
         }
     }
   },
-  -- extensions = {
-  --     frecency = {},
-  -- }
+    extensions = {
+        fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = true,  -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+        }
+    },
 }
 
--- require"telescope".load_extension("frecency")
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
 
 -- vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 vim.api.nvim_set_keymap('i', '<C-l>', '<CMD>lua require\'telescope.builtin\'.buffers({sort_mru = true, sort_lastused = true})<CR>', { silent = true })
@@ -75,7 +83,8 @@ vim.api.nvim_set_keymap('n', '<C-p>', '<CMD>lua require\'telescope.builtin\'.git
 
 -- vim.cmd('command Rg execute "lua require\'telescope.builtin\'.live_grep()"')
 -- vim.cmd('command Ag execute "lua require\'telescope.builtin\'.live_grep()"')
-vim.cmd('command Lg execute "lua require\'telescope.builtin\'.live_grep()"')
+vim.cmd('command Lg execute "lua require\'telescope.builtin\'.live_grep()<CR>"')
+vim.cmd('command Lgs execute "lua require\'telescope.builtin\'.grep_string()<CR>"')
 
 -- vim.api.nvim_set_keymap('i', '<leader>fg', '<CMD>lua require\'telescope.builtin\'.live_grep()<CR>', { silent = true })
 -- vim.api.nvim_set_keymap('n', '<leader>fg', '<CMD>lua require\'telescope.builtin\'.live_grep()<CR>', { silent = true })
