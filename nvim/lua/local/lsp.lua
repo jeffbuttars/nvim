3e067c521cfc333bb887e192ca6af6acc26bc5c1
Jeff Buttars <jeffbuttars@gmail.com>
2021-11-12 07:50:22 -0700

    wip

--- nvim/lua/local/lsp.lua
+++ nvim/lua/local/lsp.lua
@@ -14,6 +14,14 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   }
 )
 
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+
 -- require'lspconfig'.pyright.setup{}
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
--- nvim/lua/local/lualine.lua
+++ nvim/lua/local/lualine.lua
@@ -1,5 +1,13 @@
 -- https://github.com/hoob3rt/lualine.nvim
 
+-- local custom_gruvbox = require'lualine.themes.gruvbox'
+-- -- Change the background of lualine_c section for normal mode
+-- custom_gruvbox.normal.c.bg = '#112233' -- rgb colors are supported
+-- require'lualine'.setup{
+--   options = { theme  = custom_gruvbox },
+--   ...
+-- }
+
 local l_options = {
   section_separators = {'', ''},
   -- component_separators = {'', ''}
@@ -15,3 +23,4 @@ require('lualine').setup({
     options = l_options,
     extension = {'quickfix', 'fugitive', 'fzf', 'nvim-tree'}
 })
+
