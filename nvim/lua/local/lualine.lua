c7b1e74f4ef542f30ef9f41115b2547a9cc97f76
Jeff Buttars <jeffbuttars@gmail.com>
2021-11-14 17:35:55 -0700

    Got the lualine colors to how I like them

--- nvim/lua/local/lualine.lua
+++ nvim/lua/local/lualine.lua
@@ -1,9 +1,19 @@
 -- local my_utils = require('local.utils')
 local nf_icons = require('local.nf-icons')
-local ll_utils = require 'lualine.utils.utils'
+
+-- Setup and customize our theme
+local current_theme_name = 'solarized_dark'
+if vim.env.TERM_META == 'dark' then
+    current_theme_name = 'solarized_dark'
+else
+    current_theme_name = 'solarized_light'
+end
 
 local current_theme_name = 'solarized_dark'
 local current_theme = require('lualine.themes' .. '.' .. current_theme_name)
+
+-- make the colors the same on boths sides of the line
+current_theme.normal.b = current_theme.normal.c
 
 -- https://github.com/hoob3rt/lualine.nvim
 
@@ -16,17 +26,11 @@ local current_theme = require('lualine.themes' .. '.' .. current_theme_name)
 -- }
 
 local l_options = {
-  section_separators = {nf_icons['ple-right_half_circle_thick'], nf_icons['ple-left_half_circle_thick']},
+  -- section_separators = {nf_icons['ple-right_half_circle_thick'], nf_icons['ple-left_half_circle_thick']},
   -- section_separators = {my_utils.icons.half_circle_right_filled, my_utils.icons.half_circle_left_filled},
-  -- component_separators = {my_utils.icons.cresent_circle_right, my_utils.icons.cresent_circle_left}
-
+  -- component_separators = {nf_icons['ple-right_half_circle_thick'], nf_icons['ple-left_half_circle_thick']}
+  theme = current_theme
 }
-
-if vim.env.TERM_META == 'dark' then
-    l_options.theme = 'solarized_dark'
-else
-    l_options.theme = 'solarized_light'
-end
 
 require('lualine').setup({
     options = l_options,
@@ -34,26 +38,8 @@ require('lualine').setup({
     sections = {
       lualine_b = {
           'hostname',
-          {
-            'branch',
-            color = {
-                  bg = current_theme.normal.c.bg,
-                  fg =  current_theme.normal.c.fg,
-            },
-            separator = {
-                left = nf_icons['pl-left_soft_divider'],
-                right = nf_icons['pl-left_soft_divider'],
-            },
-          },
-          {
-            'filename',
-            color = {
-                  bg = current_theme.normal.c.bg,
-                  fg =  current_theme.normal.c.fg,
-            },
-            separator = nf_icons['ple-right_half_circle_thick'],
-          },
-          -- 'diff',
+          'branch',
+          'filename',
          {
           'diagnostics',
     --       -- table of diagnostic sources, available sources:
@@ -63,29 +49,6 @@ require('lualine').setup({
           sources = {'nvim_lsp'},
     --       -- displays diagnostics from defined severity
           sections = {'error', 'warn', 'info', 'hint'},
-          diagnostics_color = {
-              error = {
-                  bg = current_theme.normal.c.bg
-              },
-              warn = {
-                  bg = current_theme.normal.c.bg
-              },
-              info = {
-                  bg = current_theme.normal.c.bg
-              },
-              hint = {
-                  bg = current_theme.normal.c.bg
-              },
-          --   -- Same values like general color option can be used here.
-          --   error = 'DiagnosticError', -- changes diagnostic's error color
-          --   warn  = 'DiagnosticWarn',  -- changes diagnostic's warn color
-          --   info  = 'DiagnosticInfo',  -- Changes diagnostic's info color
-          --   hint  = 'DiagnosticHint',  -- Changes diagnostic's hint color
-          },
-    -- --       symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
-          colored = true, -- displays diagnostics status in color if set to true
-          update_in_insert = false, -- Update diagnostics in insert mode
-          always_visible = false, -- Show diagnostics even if count is 0, boolean or function returning boolean
         }
       },
 
