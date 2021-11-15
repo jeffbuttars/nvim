01e538bc6ff73b2ac0d648454ce2af1347a32bf8
Jeff Buttars <jeffbuttars@gmail.com>
2021-11-15 14:59:34 -0700

    tweek lua line colors a bit

--- nvim/lua/local/lualine.lua
+++ nvim/lua/local/lualine.lua
@@ -1,5 +1,6 @@
 -- local my_utils = require('local.utils')
 local nf_icons = require('local.nf-icons')
+local colors = require('local.utils').solarized_colors
 
 -- Setup and customize our theme
 local current_theme_name = 'solarized_dark'
@@ -12,8 +13,16 @@ end
 local current_theme_name = 'solarized_dark'
 local current_theme = require('lualine.themes' .. '.' .. current_theme_name)
 
--- make the colors the same on boths sides of the line
-current_theme.normal.b = current_theme.normal.c
+
+if vim.env.TERM_META == 'dark' then
+    -- make the colors the same on boths sides of the line
+    -- current_theme.normal.b = current_theme.normal.c
+    current_theme.normal.b.bg = colors.base02
+    current_theme.normal.b.fg = colors.base0
+
+    current_theme.normal.c.bg = colors.base03
+    current_theme.normal.c.fg = colors.base1
+end
 
 -- https://github.com/hoob3rt/lualine.nvim
 
@@ -26,14 +35,13 @@ current_theme.normal.b = current_theme.normal.c
 -- }
 
 local l_options = {
-  -- section_separators = {nf_icons['ple-right_half_circle_thick'], nf_icons['ple-left_half_circle_thick']},
+  section_separators = { left = nf_icons['ple-right_half_circle_thick'], right = nf_icons['ple-left_half_circle_thick'] },
   -- section_separators = {my_utils.icons.half_circle_right_filled, my_utils.icons.half_circle_left_filled},
   -- component_separators = {nf_icons['ple-right_half_circle_thick'], nf_icons['ple-left_half_circle_thick']}
 
   -- 'pl-right_soft_divider' => ''
   -- 'pl-left_soft_divider' => ''
-  -- component_separators = {nf_icons['pl-left_soft_divider'], nf_icons['pl-right_soft_divider']},
-  --pl-left_soft_divider
+  -- component_separators = { left = nf_icons['pl-left_soft_divider'], right = nf_icons['pl-right_soft_divider'] },
   theme = current_theme
 }
 
@@ -44,20 +52,29 @@ require('lualine').setup({
       lualine_b = {
           'hostname',
           'branch',
-          'filename',
       },
-
       lualine_c = {
-         {
-          'diagnostics',
-    --       -- table of diagnostic sources, available sources:
-    --       -- 'nvim_lsp', 'nvim', 'coc', 'ale', 'vim_lsp'
-    --       -- Or a function that returns a table like
-    --       --   {error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt}
-          sources = {'nvim_lsp'},
-    --       -- displays diagnostics from defined severity
-          sections = {'error', 'warn', 'info', 'hint'},
-        }
+          {
+            'filename',
+            separator = nf_icons['oct-dash'],
+            -- separator = ':',
+          },
+          {
+            'filesize',
+            separator = ' ' .. nf_icons['oct-bug'],
+            -- separator = nf_icons['pl-left_soft_divider'],
+          },
+          {
+            'diagnostics',
+            -- table of diagnostic sources, available sources:
+            -- 'nvim_lsp', 'nvim', 'coc', 'ale', 'vim_lsp'
+            -- Or a function that returns a table like
+            --   {error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt}
+            sources = {'nvim_lsp'},
+            -- displays diagnostics from defined severity
+            sections = {'error', 'warn', 'info', 'hint'},
+            separator = nf_icons['pl-left_soft_divider'],
+          }
       },
       -- lualine_x = {'encoding', 'fileformat', 'filetype'},
       lualine_x = {},
--- nvim/lua/local/utils.lua
+++ nvim/lua/local/utils.lua
@@ -8,6 +8,24 @@ local utils = {
     information = nf_icons['oct-info'], -- 
     hint = nf_icons['fa-hand_o_right'], -- 
     other = nf_icons['mdi-check_circle_outline'], -- 﫠
+  },
+  solarized_colors = {
+    base03  =  '#002b36',
+    base02  =  '#073642',
+    base01  =  '#586e75',
+    base00  =  '#657b83',
+    base0   =  '#839496',
+    base1   =  '#93a1a1',
+    base2   =  '#eee8d5',
+    base3   =  '#fdf6e3',
+    yellow  =  '#b58900',
+    orange  =  '#cb4b16',
+    red     =  '#dc322f',
+    magenta =  '#d33682',
+    violet  =  '#6c71c4',
+    blue    =  '#268bd2',
+    cyan    =  '#2aa198',
+    green   =  '#859900',
   }
 }
 
