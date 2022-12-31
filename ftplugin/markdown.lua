11ad8faca121804b2ab0f5b7c3b8bc22107dc1cb
Jeff Buttars <jeffbuttars@gmail.com>
2022-12-31 14:50:09 -0700

    Add some ftplugin files

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,5 @@
+
+-- https://github.com/f3fora/cmp-spell
+-- Setting spell (and spelllang) is mandatory to use spellsuggest.
+vim.opt.spell = true
+vim.opt.spelllang = { 'en_us' }
---
+++ ftplugin/text.lua
@@ -0,0 +1,4 @@
+-- https://github.com/f3fora/cmp-spell
+-- Setting spell (and spelllang) is mandatory to use spellsuggest.
+vim.opt.spell = true
+vim.opt.spelllang = { 'en_us' }
