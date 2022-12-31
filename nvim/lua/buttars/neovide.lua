ca732ab8d21ad182c7c0f878b8eb9605e018b033
Jeff Buttars <jeffbuttars@gmail.com>
2022-12-30 21:19:19 -0700

    Add gui font adjustments when using neovide

--- nvim/lua/buttars/neovide.lua
+++ nvim/lua/buttars/neovide.lua
@@ -16,4 +16,34 @@ vim.g.neovide_floating_blur_amount_x = 2.0
 vim.g.neovide_floating_blur_amount_y = 2.0
 
 -- set guifont=FiraCode\ Nerd\ Font\ Mono:style=Medium\\,Regular:h24
-vim.opt.guifont = {"FiraCode Nerd Font Mono", ":h10"}
+-- vim.opt.guifont = {"FiraCode Nerd Font Mono", ":h10"}
+
+-- https://neovide.dev/faq.html
+-- Adjust the font size while running
+vim.g.gui_font_default_size = 12
+vim.g.gui_font_size = vim.g.gui_font_default_size
+vim.g.gui_font_face = "FiraCode Nerd Font Mono"
+
+local RefreshGuiFont = function()
+  -- vim.opt.guifont = string.format("%s:h%s",vim.g.gui_font_face, vim.g.gui_font_size)
+  vim.opt.guifont = {vim.g.gui_font_face, string.format(":h%s", vim.g.gui_font_size)}
+end
+
+local ResizeGuiFont = function(delta)
+  vim.g.gui_font_size = vim.g.gui_font_size + delta
+  RefreshGuiFont()
+end
+
+local ResetGuiFont = function()
+  vim.g.gui_font_size = vim.g.gui_font_default_size
+  RefreshGuiFont()
+end
+
+-- Call function on startup to set default value
+ResetGuiFont()
+
+-- Keymaps
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set({'n', 'i'}, "<C-=>", function() ResizeGuiFont(1)  end, opts)
+vim.keymap.set({'n', 'i'}, "<C-->", function() ResizeGuiFont(-1) end, opts)
