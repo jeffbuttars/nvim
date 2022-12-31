88905460c22d6a799230952e905a7cd400884b31
Jeff Buttars <jeffbuttars@gmail.com>
2022-12-31 14:52:01 -0700

    Remove pylint from null-ls

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -20,7 +20,6 @@ null_ls.setup({
 		-- null_ls.builtins.diagnostics.markdownlint_cli2
 		null_ls.builtins.diagnostics.mypy,
 		null_ls.builtins.diagnostics.pydocstyle,
-		null_ls.builtins.diagnostics.pylint,
 		null_ls.builtins.diagnostics.ruff,
 		null_ls.builtins.diagnostics.shellcheck,
 		null_ls.builtins.diagnostics.sqlfluff.with({
