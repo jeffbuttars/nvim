5720df73cd8572a2b5ed3dcadef1518255a85a74
Jeff Buttars <jeffbuttars@gmail.com>
2023-01-02 12:47:39 -0700

    Open and focus neo-tree with mapping, don't close it

--- after/plugin/neo-tree.lua
+++ after/plugin/neo-tree.lua
@@ -1,20 +1,14 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 require("neo-tree").setup({
-    filesystem = {
-        follow_current_file = true,
-        use_libuv_file_watcher = true,
-    },
+	filesystem = {
+		follow_current_file = true,
+		use_libuv_file_watcher = true,
+	},
 
-    buffers = {
-        follow_current_file = true,
-    },
+	buffers = {
+		follow_current_file = true,
+	},
 })
 
-
-vim.api.nvim_set_keymap(
-    "n",
-    "<C-d>",
-    ":Neotree reveal action=focus toggle<CR>",
-    { noremap = true, silent = true }
-)
+vim.api.nvim_set_keymap("n", "<C-d>", ":NeoTreeFocus<CR>", { noremap = true, silent = true })
