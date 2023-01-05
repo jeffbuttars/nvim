40656b285238338756dd3a7b0beb95b13c59689c
Jeff Buttars <jeffbuttars@gmail.com>
2023-01-05 12:28:18 -0700

    Ignore treesitter parser t32

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -11,10 +11,11 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 
   -- List of parsers to ignore installing (for "all")
-  -- ignore_install = { "javascript" },
+  ignore_install = { "t32" },
 
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+  ---- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+  -- parser_install_dir = "/some/path/to/store/parsers",
 
   highlight = {
     -- `false` will disable the whole extension
