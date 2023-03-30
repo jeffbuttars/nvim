9d9c0693b89d4f85543426c486e6c06d9d3a40ba
Jeff Buttars <jeffbuttars@gmail.com>
2023-03-30 10:24:31 +0800

    Update settings for deadcolumn

--- after/plugin/deadcolumn.lua
+++ after/plugin/deadcolumn.lua
@@ -1,8 +1,15 @@
 -- https://github.com/Bekaboo/deadcolumn.nvim
 local opts = {
-    threshold = 0.75,
-    scope = 'line',
-    modes = { 'i', 'ic', 'ix', 'R', 'Rc', 'Rx', 'Rv', 'Rvc', 'Rvx' },
+    scope = 'visible',  -- line, buffer, visible, cursor
+    modes = { 'i', 'ic', 'ix', 'R', 'Rc', 'Rx', 'Rv', 'Rvc', 'Rvx', 'n' },
+    blending = {
+        threshold = 0.75,
+        colorcode = '#000000',
+        hlgroup = {
+            'Normal',
+            'background',
+        },
+    },
     warning = {
         alpha = 0.4,
         colorcode = '#FF0000',
