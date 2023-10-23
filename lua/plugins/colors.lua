540bd50e948d3be5d49112d3fb98f9da9916b80b
Jeff Buttars <jeffbuttars@gmail.com>
2023-10-23 16:45:29 -0600

    wip

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,8 +1,16 @@
 return {
-{
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-latte",
     },
-  }
+  },
+  { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+
+  {
+    "folke/lsp-colors.nvim",
+  },
+  {
+    "brenoprata10/nvim-highlight-colors",
+  },
 }
