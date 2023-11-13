29f27e318cd69c9ef7b0aa3c7a810a3bf0954568
Jeff Buttars <jeffbuttars@gmail.com>
2023-11-13 07:16:11 -0700

    Comment out option that was set to the default any way

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,7 +6,7 @@
 vim.g.python3_host_prog = "~/.venv/bin/python3"
 
 -- Disable LazyVim auto format, default is true
--- vim.g.autoformat = false
+-- vim.g.autoformat = true
 
 -- This may be annoying, set to false if so
 -- opt.confirm = true -- Confirm to save changes before exiting modified buffer
