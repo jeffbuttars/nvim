53e31cc20a7cb47504de0a3bb1eadf7cba322bd0
Jeff Buttars <jeffbuttars@gmail.com>
2023-11-13 07:30:08 -0700

    Re-enabling some plugins to see how it goes

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -15,38 +15,50 @@ return {
     "echasnovski/mini.ai",
     enabled = false,
   },
-  {
-    "folke/flash.nvim",
-    enabled = false,
-  },
-  {
-    "RRethy/vim-illuminate",
-    enabled = false,
-  },
+  -- {
+  --   "folke/flash.nvim",
+  --   enabled = false,
+  -- },
+  -- {
+  --   "RRethy/vim-illuminate",
+  --   enabled = false,
+  -- },
 
   {
     "echasnovski/mini.bufremove",
     enabled = false,
   },
+  -- {
+  --   "folke/todo-comments.nvim",
+  --   enabled = false,
+  -- },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter-textobjects",
+  --   enabled = false,
+  -- },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter-context",
+  --   enabled = false,
+  -- },
   {
-    "folke/todo-comments.nvim",
-    enabled = false,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    enabled = false,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
+    "nvimdev/dashboard-nvim",
     enabled = false,
   },
   -- {
   --   "rcarriga/nvim-notify",
   --   enabled = false,
   -- },
+  -- {
+  --   -- Looks cool, not sure how slow it is, might want to re-enable
+  --   "echasnovski/mini.indentscope",
+  --   enabled = false,
+  -- },
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   enabled = false,
+  -- },
   {
-    -- Looks cool, not sure how slow it is, might want to re-enable
-    "echasnovski/mini.indentscope",
+    "dstein64/vim-startuptime",
     enabled = false,
   },
   -- { "MunifTanjim/nui.nvim", lazy = true, enabled = false },
