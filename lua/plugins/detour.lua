df98003fe351c94dc7499c3675ac9042383bc543
Jeff Buttars <jeffbuttars@gmail.com>
2023-11-20 08:29:06 -0700

    wip

--- lua/plugins/detour.lua
+++ lua/plugins/detour.lua
@@ -1,23 +1,43 @@
 return {
   {
     "carbon-steel/detour.nvim",
-    -- config = function()
-    --   -- A keymap for selecting a terminal buffer to open in a popup
-    --   local wk = require("which-key")
-    --     -- ["<leader>gd"] = {
-    --     --   function()
-    --     --     local gs = package.loaded.gitsigns
-    --     --     require("detour").Detour() -- Open a detour popup
-    --     --
-    --     --     -- Switch to a blank buffer to prevent any accidental changes.
-    --     --     -- vim.cmd.new()
-    --     --     vim.bo.bufhidden = "delete"
-    --     --
-    --     --     gs.diffthis("~")
-    --     --   end,
-    --     --   " Diffthis",
-    --     -- },
-    --   })
-    -- end,
+    config = function()
+      -- A keymap for selecting a terminal buffer to open in a popup
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>bd"] = {
+          function()
+            require("detour").Detour() -- Open a detour popup
+          end,
+          "Detour",
+        },
+        -- ["<leader>bt"] = {
+        --   function()
+        --     require("detour").Detour() -- Open a detour popup
+        --
+        --     -- Switch to a blank buffer to prevent any accidental changes.
+        --     -- vim.cmd.new()
+        --     -- vim.bo.bufhidden = "delete"
+        --
+        --     require("neo-tree.command").execute({ source = "git_status", toggle = true })
+        --   end,
+        --   "Buffer Tree",
+        -- },
+
+        -- ["<leader>gd"] = {
+        --   function()
+        --     local gs = package.loaded.gitsigns
+        --     require("detour").Detour() -- Open a detour popup
+        --
+        --     -- Switch to a blank buffer to prevent any accidental changes.
+        --     -- vim.cmd.new()
+        --     vim.bo.bufhidden = "delete"
+        --
+        --     gs.diffthis("~")
+        --   end,
+        --   " Diffthis",
+        -- },
+      })
+    end,
   },
 }
