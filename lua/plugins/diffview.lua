e426c234eea579c9d5ac8c0c7c036520e028f693
Jeff Buttars <jeffbuttars@gmail.com>
2024-03-02 09:55:58 -0700

    wop

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,8 +1,16 @@
 return {
-    {
-        -- https://github.com/sindrets/diffview.nvim
-        "sindrets/diffview.nvim",
-        dependencies = {"nvim-lua/plenary.nvim"},
-        lazy = true,
+  {
+    -- https://github.com/sindrets/diffview.nvim
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = {
+      "DiffviewClose",
+      "DiffviewFileHistory",
+      "DiffviewFocusFiles",
+      "DiffviewLog",
+      "DiffviewOpen",
+      "DiffviewRefresh",
+      "DiffviewToggleFiles",
     },
+  },
 }
