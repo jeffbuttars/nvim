8482761ef5299f5645af72f79856871c9d1f87b2
Jeff Buttars <jeffbuttars@gmail.com>
2024-06-04 08:29:57 -0600

    disable inlay_hints in lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,6 +5,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       diagnostics = { virtual_text = false, float = { border = "rounded" } },
+      inlay_hints = { enabled = false },
 
       -- servers = {
       --   typos_lsp = {},
