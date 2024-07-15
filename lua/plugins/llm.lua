1ac642a0c3dd082a838412f65b759fdd26e8b0d7
Jeff Buttars <jeffbuttars@gmail.com>
2024-07-15 14:17:15 -0600

    add svelte to lang. update whichkey configs

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -15,39 +15,62 @@ return {
       -- Setup up some keymaps
       local wk = require("which-key")
 
-      wk.register({
-
-        ["<leader>a"] = {
-          name = "AI",
+      wk.add({
+        { "<leader>a", group = "AI" },
+        {
           mode = { "n", "v" },
-          c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
-          z = { "<cmd>ChatGPTActAs<CR>", "ChatGPTActAs", mode = { "n" } },
-          e = {
-            "<cmd>ChatGPTEditWithInstruction<CR>",
-            "Edit with instruction",
-            mode = { "n", "v" },
-          },
-          g = {
-            "<cmd>ChatGPTRun grammar_correction<CR>",
-            "Grammar Correction",
-            mode = { "n", "v" },
-          },
-          t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
-          k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
-          d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
-          a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
-          o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
-          s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
-          f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
-          x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
-          r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
-          l = {
+          { "<leader>ac", "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
+          { "<leader>az", "<cmd>ChatGPTActAs<CR>", desc = "ChatGPTActAs", mode = { "n" } },
+          { "<leader>ae", "<cmd>ChatGPTEditWithInstruction<CR>", desc = "Edit with instruction" },
+          { "<leader>ag", "<cmd>ChatGPTRun grammar_correction<CR>", desc = "Grammar Correction" },
+          { "<leader>at", "<cmd>ChatGPTRun translate<CR>", desc = "Translate" },
+          { "<leader>aa", "<cmd>ChatGPTRun add_tests<CR>", desc = "Add Tests" },
+          { "<leader>ad", "<cmd>ChatGPTRun docstring<CR>", desc = "Docstring" },
+          { "<leader>af", "<cmd>ChatGPTRun fix_bugs<CR>", desc = "Fix Bugs" },
+          { "<leader>ak", "<cmd>ChatGPTRun keywords<CR>", desc = "Keywords" },
+          {
+            "<leader>al",
             "<cmd>ChatGPTRun code_readability_analysis<CR>",
-            "Code Readability Analysis",
-            mode = { "n", "v" },
+            desc = "Code Readability Analysis",
           },
+          { "<leader>ao", "<cmd>ChatGPTRun optimize_code<CR>", desc = "Optimize Code" },
+          { "<leader>ar", "<cmd>ChatGPTRun roxygen_edit<CR>", desc = "Roxygen Edit" },
+          { "<leader>as", "<cmd>ChatGPTRun summarize<CR>", desc = "Summarize" },
+          { "<leader>ax", "<cmd>ChatGPTRun explain_code<CR>", desc = "Explain Code" },
         },
       })
+      --   , {
+      --   ["<leader>a"] = {
+      --     name = "AI",
+      --     mode = { "n", "v" },
+      --     c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+      --     z = { "<cmd>ChatGPTActAs<CR>", "ChatGPTActAs", mode = { "n" } },
+      --     e = {
+      --       "<cmd>ChatGPTEditWithInstruction<CR>",
+      --       "Edit with instruction",
+      --       mode = { "n", "v" },
+      --     },
+      --     g = {
+      --       "<cmd>ChatGPTRun grammar_correction<CR>",
+      --       "Grammar Correction",
+      --       mode = { "n", "v" },
+      --     },
+      --     t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+      --     k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+      --     d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+      --     a = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+      --     o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+      --     s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+      --     f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+      --     x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+      --     r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+      --     l = {
+      --       "<cmd>ChatGPTRun code_readability_analysis<CR>",
+      --       "Code Readability Analysis",
+      --       mode = { "n", "v" },
+      --     },
+      --   },
+      -- }
     end,
     opts = {
       api_key_cmd = nil,
