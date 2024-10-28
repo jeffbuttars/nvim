8bdf017481f400096dcb0033028333401153ca9f
Jeff Buttars <jeffbuttars@gmail.com>
2024-10-28 11:21:23 -0600

    Add mason.lua to add our own list of ensure_installed

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,25 @@
+return {
+  "williamboman/mason.nvim",
+
+  opts_extend = { "ensure_installed" },
+  opts = {
+    ensure_installed = {
+      "codespell",
+      "cueimports",
+      "cuelsp",
+      "cuepls",
+      "golines",
+      "gomodifytags",
+      "htmlbeautifier",
+      "htmlhint",
+      "html-lsp",
+      "jedi-language-server",
+      "mypy",
+      "shfmt",
+      "standardjs",
+      "stylua",
+      "templ",
+      "typos",
+    },
+  },
+}
