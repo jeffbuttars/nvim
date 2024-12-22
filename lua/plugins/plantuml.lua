c52570de65c55615fc79ee8b1b84b7ca35945d62
Jeff Buttars <jeffbuttars@gmail.com>
2024-12-22 09:10:42 -0700

    plantuml

--- lua/plugins/plantuml.lua
+++ lua/plugins/plantuml.lua
@@ -5,5 +5,9 @@ return {
       "tyru/open-browser.vim",
       "aklt/plantuml-syntax",
     },
+
+    init = function()
+      vim.g["plantuml_previewer#plantuml_jar_path"] = "/usr/share/java/plantuml/plantuml.jar"
+    end,
   },
 }
