1c32a9873864da00a2e036cf20fe5f6baf181910
Jeff Buttars <jeffbuttars@gmail.com>
2025-01-01 12:39:15 -0700

    master

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -63,11 +63,11 @@ require("lazy").setup({
       },
     },
   },
-  dev = {
-    -- directory where you store your local plugin projects
-    path = "~/Dev",
-    ---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
-    -- patterns = {}, -- For example {"folke"}
-    fallback = false, -- Fallback to git when local plugin doesn't exist
-  },
+  -- dev = {
+  --   -- directory where you store your local plugin projects
+  --   path = "~/Dev",
+  --   ---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
+  --   -- patterns = {}, -- For example {"folke"}
+  --   fallback = false, -- Fallback to git when local plugin doesn't exist
+  -- },
 })
