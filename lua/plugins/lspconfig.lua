4f894841a94b05473b02c8df4dfc3fedb53b2fb6
Jeff Buttars <jeffbuttars@gmail.com>
2025-03-31 07:55:58 -0600

    Updated the lazy-lock.json file with new commits for several plugins, and modified lua/plugins/lspconfig.lua to add configuration for the Postgres LSP server. Commented out sections in lspconfig.lua to refine configurations for various language servers.
    *lazy-lock.json*
    - Updated commit hashes for several plugins including SchemaStore.nvim, codecompanion.nvim, conform.nvim, e-ink.nvim, grug-far.nvim, nvim-lspconfig, nvim-treesitter, oil.nvim, and render-markdown.nvim.
    
    *lua/plugins/lspconfig.lua*
    - Added configuration for the Postgres LSP server.
    - Commented out unused configurations for the Python LSP setup.
    - Maintained structure in preparation for additional custom server configurations.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,96 +1,102 @@
--- require("lspconfig").jedi_language_server.setup({})
+require("lspconfig").postgres_lsp.setup({})
 
 return {
-  {
-    -- https://www.lazyvim.org/extras/lang/python
-    "neovim/nvim-lspconfig",
+  -- {
+  -- https://www.lazyvim.org/extras/lang/python
+  -- "neovim/nvim-lspconfig",
 
-    -- init = function()
-    --   require'lspconfig'.gopls.setup{}
-    -- end,
-    -- keys = {
-    --   {
-    --     "K",
-    --     function()
-    --       res = vim.lsp.buf.hover()
-    --       vim.print("HOMVER RES:", res)
-    --       return res
-    --     end,
-    --     desc = "Hover",
-    --     mode = {"n"},
-    --   },
-    -- },
+  -- init = function()
+  --   require'lspconfig'.gopls.setup{}
+  -- end,
+  -- keys = {
+  --   {
+  --     "K",
+  --     function()
+  --       res = vim.lsp.buf.hover()
+  --       vim.print("HOMVER RES:", res)
+  --       return res
+  --     end,
+  --     desc = "Hover",
+  --     mode = {"n"},
+  --   },
+  -- },
 
-    opts = function(_, opts)
-      -- local servers = { "pyright", "basedpyright", "ruff", "ruff_lsp", ruff, lsp }
-      local servers =
-        { "jedi-language-server", "ruff", "ruff_lsp", opts.servers.ruff, opts.servers.lsp }
-      for _, server in ipairs(servers) do
-        opts.servers[server] = opts.servers[server] or {}
-        opts.servers[server].enabled = server == lsp or server == ruff
-      end
+  -- opts = function(_, opts)
+  -- local servers = { "pyright", "basedpyright", "ruff", "ruff_lsp", ruff, lsp }
+  -- local servers = {
+  --   "jedi-language-server",
+  --   "ruff",
+  --   "ruff_lsp",
+  --   opts.servers.ruff,
+  --   opts.servers.lsp,
+  --   "postgres_lsp",
+  -- }
+  -- for _, server in ipairs(servers) do
+  --   opts.servers[server] = opts.servers[server] or {}
+  --   opts.servers[server].enabled = server == "lsp" or server == "ruff"
+  -- end
 
-      opts.diagnostics = { virtual_text = false, float = { border = "rounded" } }
-      opts.inlay_hints = { enabled = false }
-    end,
+  -- opts.diagnostics = { virtual_text = false, float = { border = "rounded" } }
+  -- opts.inlay_hints = { enabled = false }
+  -- end,
 
-    --  opts = {
-    --    diagnostics = { virtual_text = false, float = { border = "rounded" } },
-    --    inlay_hints = { enabled = false },
+  --  opts = {
+  --    diagnostics = { virtual_text = false, float = { border = "rounded" } },
+  --    inlay_hints = { enabled = false },
 
-    --    -- servers = {
-    --    --   typos_lsp = {},
-    --    --   -- pylsp = {
-    --    --   --   settings = {
-    --    --   --     formatCommand = { "black" },
-    --    --   --     pylsp = {
-    --    --   --       plugins = {
-    --    --   --         jedi_completion = {
-    --    --   --           enabled = true,
-    --    --   --           fuzzy = true,
-    --    --   --         },
-    --    --   --         pylsp_mypy = {
-    --    --   --           enabled = true,
-    --    --   --         },
-    --    --   --         -- ruff = {
-    --    --   --         --   enabled = true,
-    --    --   --         --   config = "~/pyproject.toml",
-    --    --   --         --   -- extendSelect = {"I"},
-    --    --   --         -- },
-    --    --   --         black = {
-    --    --   --           enabled = true,
-    --    --   --           lineLength = 99,
-    --    --   --           -- preview = true
-    --    --   --         },
-    --    --   --         isort = {
-    --    --   --           enabled = true,
-    --    --   --           profile = "black",
-    --    --   --         },
-    --    --   --         autopep8 = {
-    --    --   --           enabled = false,
-    --    --   --         },
-    --    --   --         pyflakes = {
-    --    --   --           enabled = false,
-    --    --   --         },
-    --    --   --         pycodestyle = {
-    --    --   --           enabled = false,
-    --    --   --           ignore = { "W391" },
-    --    --   --           maxLineLength = 100,
-    --    --   --         },
-    --    --   --         rope_autoimport = {
-    --    --   --           enabled = false,
-    --    --   --         },
-    --    --   --         yapf = {
-    --    --   --           enabled = false,
-    --    --   --         },
-    --    --   --         mccabe = {
-    --    --   --           enabled = false,
-    --    --   --         },
-    --    --   --       },
-    --    --   --     },
-    --    --   --   },
-    --    --   -- },
-    --    -- },
-    --  },
-  },
+  --    -- servers = {
+  --    --   typos_lsp = {},
+  --    --   -- pylsp = {
+  --    --   --   settings = {
+  --    --   --     formatCommand = { "black" },
+  --    --   --     pylsp = {
+  --    --   --       plugins = {
+  --    --   --         jedi_completion = {
+  --    --   --           enabled = true,
+  --    --   --           fuzzy = true,
+  --    --   --         },
+  --    --   --         pylsp_mypy = {
+  --    --   --           enabled = true,
+  --    --   --         },
+  --    --   --         -- ruff = {
+  --    --   --         --   enabled = true,
+  --    --   --         --   config = "~/pyproject.toml",
+  --    --   --         --   -- extendSelect = {"I"},
+  --    --   --         -- },
+  --    --   --         black = {
+  --    --   --           enabled = true,
+  --    --   --           lineLength = 99,
+  --    --   --           -- preview = true
+  --    --   --         },
+  --    --   --         isort = {
+  --    --   --           enabled = true,
+  --    --   --           profile = "black",
+  --    --   --         },
+  --    --   --         autopep8 = {
+  --    --   --           enabled = false,
+  --    --   --         },
+  --    --   --         pyflakes = {
+  --    --   --           enabled = false,
+  --    --   --         },
+  --    --   --         pycodestyle = {
+  --    --   --           enabled = false,
+  --    --   --           ignore = { "W391" },
+  --    --   --           maxLineLength = 100,
+  --    --   --         },
+  --    --   --         rope_autoimport = {
+  --    --   --           enabled = false,
+  --    --   --         },
+  --    --   --         yapf = {
+  --    --   --           enabled = false,
+  --    --   --         },
+  --    --   --         mccabe = {
+  --    --   --           enabled = false,
+  --    --   --         },
+  --    --   --       },
+  --    --   --     },
+  --    --   --   },
+  --    --   -- },
+  --    -- },
+  --  },
+  -- },
 }
