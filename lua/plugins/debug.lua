256292b122d1ee2a56f0494de8f61877b05b69a1
Jeff Buttars <jeffbuttars@gmail.com>
2025-05-05 08:35:55 -0600

    Here's a commit message following the conventional commit format for your changes:
    ```
    feat(plugins): add debugmaster.nvim and configure debug setup
    
    - Add debugmaster.nvim plugin for improved debugging experience
    - Create new debug.lua plugin configuration file
    - Disable nvim-dap-ui to avoid conflicts with debugmaster
    - Configure keybindings for debugmaster mode toggle
    - Update various plugin dependencies in lazy-lock.json
    ```

---
+++ lua/plugins/debug.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    -- don't mix dap-ui with debugmaster
+    "rcarriga/nvim-dap-ui",
+    enabled = false,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      -- Configure your debug adapters here
+      -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
+    end,
+  },
+  {
+    "miroshQa/debugmaster.nvim",
+    config = function()
+      local dm = require("debugmaster")
+      -- https://github.com/miroshQa/debugmaster.nvim
+      -- make sure you don't have any other keymaps that starts with "<leader>d" to avoid delay
+      vim.keymap.set({ "n", "v" }, "<leader>d", dm.mode.toggle, { nowait = true })
+      -- vim.keymap.set("t", "<C-/>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+    end,
+  },
+}
