132614c8e817ea62b17081986508d7f1ffa93496
Jeff Buttars <jeffbuttars@gmail.com>
2025-05-28 15:57:27 -0600

    Based on the provided git diff, here's a conventional commit message:
    ```
    chore(deps): update lazy-lock.json and snippets configuration
    
    - Update various plugin commits in lazy-lock.json
    - Add highlight group configuration for mini.snippets
    ```
    
    Reasoning:
    - The changes are primarily updates to locked plugin versions in lazy-lock.json
    - There's a minor configuration change in the snippets.lua file
    - The commit type is `chore` since it's routine dependency updates and configuration
    - The scope is `deps` to indicate dependency updates

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,5 +1,16 @@
 local gen_loader = require("mini.snippets").gen_loader
 
+-- # Highlight groups ~
+--
+-- * `MiniSnippetsCurrent` - current tabstop.
+-- * `MiniSnippetsCurrentReplace` - current tabstop, placeholder is to be replaced.
+-- * `MiniSnippetsFinal` - special `$0` tabstop.
+-- * `MiniSnippetsUnvisited` - not yet visited tabstop(s).
+-- * `MiniSnippetsVisited` - visited tabstop(s).
+-- vim.cmd([["hi clear MiniSnippetsUnvisited"]])
+vim.api.nvim_set_hl(0, "MiniSnippetsUnvisited", {})
+vim.api.nvim_set_hl(0, "MiniSnippetsCurrent", {})
+
 return {
   "echasnovski/mini.snippets",
   opts = {
