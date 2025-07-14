db07f878ba5ca1c6e41ab67dc832f02d2bd3cfd9
Jeff Buttars <jeffbuttars@gmail.com>
2025-07-14 09:20:02 -0600

    refactor: improve golangci-lint config and update dependencies
    - Add .claude to gitignore
    - Update plugin versions in lazy-lock.json
    - Fix lint.lua structure and enhance golangci-lint arguments
    - Remove unused fzf-lua dependency

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,86 +1,86 @@
 return {
-  "mfussenegger/nvim-lint",
-  opts = {
-    linters = {
-      golangcilint = {
-        cmd = "golangci-lint",
-        append_fname = false,
-        stream = "stdout",
-        args = {
-          "run",
-          "--output.json.path=stdout",
-          -- "--output.text.path=stderr",
-          "--issues-exit-code=0",
-          "--show-stats=false",
-          -- '--output.text.print-issued-lines=false',
-          -- '--output.text.print-linter-name=false',
-          function()
-            return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
-          end,
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        golangcilint = {
+          cmd = "golangci-lint",
+          append_fname = false,
+          stream = "stdout",
+          args = {
+            "run",
+            "--enable=revive",
+            "--output.json.path=stdout",
+            -- Overwrite values possibly set in .golangci.yml
+            "--output.text.path=",
+            "--output.tab.path=",
+            "--output.html.path=",
+            "--output.checkstyle.path=",
+            "--output.code-climate.path=",
+            "--output.junit-xml.path=",
+            "--output.teamcity.path=",
+            "--output.sarif.path=",
+            "--issues-exit-code=0",
+            "--show-stats=false",
+            -- Get absolute path of the linted file
+            "--path-mode=abs",
+            function()
+              return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+            end,
+          },
         },
       },
     },
+    -- init = function()
+    --   --   -- vim.print("LINTING")
+    --   local lint = require("lint")
+    --   vim.print("PREV LINTING", lint.linters.golangcilint.args)
+    --   lint.linters.golangcilint.args = {
+    --     "run",
+    --     "--output.json.path=stdout",
+    --     "--output.text.path=stderr",
+    --     "--issues-exit-code=0",
+    --     "--show-stats=false",
+    --     -- '--output.text.print-issued-lines=false',
+    --     -- '--output.text.print-linter-name=false',
+    --     function()
+    --       return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+    --     end,
+    --   }
+    --
+    --   vim.print("LINTING", lint.linters.golangcilint.args)
+    --   -- table.insert(golangcilint.args, #golangcilint.args, "--disable=govet.fieldalignment")
+    --   -- table.insert(golangcilint.args, #golangcilint.args, "--config ~/.golangci.yml")
+    --   --   -- vim.print("LINTING INIT:", golangcilint.args)
+    -- end,
+
+    -- opts = function(_, opts)
+    --   opts.linters.golangcilint = {
+    --       cmd = "golangci-lint",
+    --       append_fname = false,
+    --       stream = "stdout",
+    --       args = {
+    --         "run",
+    --         "--output.json.path=stdout",
+    --         "--output.text.path=stderr",
+    --         "--issues-exit-code=0",
+    --         "--show-stats=false",
+    --         -- '--output.text.print-issued-lines=false',
+    --         -- '--output.text.print-linter-name=false',
+    --         function()
+    --           return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+    --         end,
+    --       },
+    --   }
+    --   end
+
+    --   opts.linters_by_ft = {
+    -- --     sh = { "shellcheck" },
+    -- --     python = { "ruff" },
+    -- --     javascript = { "standardjs" },
+    --     go = { "golangcilint" },
+    -- --     ["*"] = { "typos", "codespell" },
+    --   }
+    -- end,
   },
-  -- init = function()
-  --   --   -- vim.print("LINTING")
-  --   local lint = require("lint")
-  --   vim.print("PREV LINTING", lint.linters.golangcilint.args)
-  --   lint.linters.golangcilint.args = {
-  --     "run",
-  --     "--output.json.path=stdout",
-  --     "--output.text.path=stderr",
-  --     "--issues-exit-code=0",
-  --     "--show-stats=false",
-  --     -- '--output.text.print-issued-lines=false',
-  --     -- '--output.text.print-linter-name=false',
-  --     function()
-  --       return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
-  --     end,
-  --   }
-  --
-  --   vim.print("LINTING", lint.linters.golangcilint.args)
-  --   -- table.insert(golangcilint.args, #golangcilint.args, "--disable=govet.fieldalignment")
-  --   -- table.insert(golangcilint.args, #golangcilint.args, "--config ~/.golangci.yml")
-  --   --   -- vim.print("LINTING INIT:", golangcilint.args)
-  -- end,
-
-  -- opts = function(_, opts)
-  --   opts.linters.golangcilint = {
-  --       cmd = "golangci-lint",
-  --       append_fname = false,
-  --       stream = "stdout",
-  --       args = {
-  --         "run",
-  --         "--output.json.path=stdout",
-  --         "--output.text.path=stderr",
-  --         "--issues-exit-code=0",
-  --         "--show-stats=false",
-  --         -- '--output.text.print-issued-lines=false',
-  --         -- '--output.text.print-linter-name=false',
-  --         function()
-  --           return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
-  --         end,
-  --       },
-  --   }
-  --   end
-
-  --   opts.linters_by_ft = {
-  -- --     sh = { "shellcheck" },
-  -- --     python = { "ruff" },
-  -- --     javascript = { "standardjs" },
-  --     go = { "golangcilint" },
-  -- --     ["*"] = { "typos", "codespell" },
-  --   }
-  -- end,
 }
--- args = {
---     'run',
---     '--output.json.path=stdout',
---     '--issues-exit-code=0',
---     '--show-stats=false',
---     '--output.text.print-issued-lines=false',
---     '--output.text.print-linter-name=false',
---     function()
---       return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
---     end
--- },
