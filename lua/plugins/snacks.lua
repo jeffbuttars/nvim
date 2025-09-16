12a77ec0c4d2a8d9ae2645474bef28993fe893d5
Jeff Buttars <jeffbuttars@gmail.com>
2025-09-16 12:42:05 -0600

    chore: update lazy-lock.json and snacks plugin config
    - Update plugin versions in lazy-lock.json
    - Reorganize commented plugin configuration for snacks.nvim
    - Add venv-selector.nvim to plugin list

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,23 +6,30 @@ return {
   -- {
   --   "folke/snacks.nvim",
   --   opts = {
-  --     animate = {
-  --       enabled = true,
-  --       -- ---@class snacks.animate.Config
-  --       -- ---@field easing? snacks.animate.easing|snacks.animate.easing.Fn
-  --       -- {
-  --       --   ---@type snacks.animate.Duration|number
-  --       --   duration = 20, -- ms per step
-  --       --   easing = "linear",
-  --       --   fps = 60, -- frames per second. Global setting for all animations
-  --       -- }
+  --     picker = {
+  --       explorer = {
+  --         follow_file = true,
+  --         watch = true,
+  --       },
   --     },
-  --     image = {
-  --       enabled = true,
-  --     },
-  --     zen = {
-  --       width = 120,
-  --     },
+  --     --     animate = {
+  --     --       enabled = true,
+  --     --       -- ---@class snacks.animate.Config
+  --     --       -- ---@field easing? snacks.animate.easing|snacks.animate.easing.Fn
+  --     --       -- {
+  --     --       --   ---@type snacks.animate.Duration|number
+  --     --       --   duration = 20, -- ms per step
+  --     --       --   easing = "linear",
+  --     --       --   fps = 60, -- frames per second. Global setting for all animations
+  --     --       -- }
+  --     --     },
+  --     --     image = {
+  --     --       enabled = true,
+  --     --     },
+  --     --     zen = {
+  --     --       width = 120,
+  --     --     },
+  --     --   },
   --   },
   -- },
 }
