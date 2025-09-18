c739347459043a0572ae4593093b944bc4dbac42
Jeff Buttars <jeffbuttars@gmail.com>
2025-09-18 08:28:31 -0600

    refactor(vim-go): adjust plugin configuration
    - Uncomment textobjects line
    - Remove commented out code
    - Simplify configuration setup

--- lua/plugins/vim-go.lua
+++ lua/plugins/vim-go.lua
@@ -124,7 +124,7 @@ return {
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 
     opts = {
-      textobjects = false, -- disable to work around treesitter.config errors
+      -- textobjects = false, -- disable to work around treesitter.config errors
       lsp_inlay_hints = { enable = false },
       -- lsp_keymaps = false,
       -- other options
@@ -132,7 +132,6 @@ return {
       -- tag_options = "",
     },
     init = function()
-
       -- local gonvim = require("go.nvim")
       -- local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
       -- vim.api.nvim_create_autocmd("BufWritePre", {
