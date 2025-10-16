d3628c2cbcbaf29fbcc2bfee1509ef015507da24
Jeff Buttars <jeffbuttars@gmail.com>
2025-10-16 09:17:03 -0600

    Based on the git diff, here's an appropriate commit message:
    ```
    chore: clean up commented code and remove unused plugin files
    
    - Remove commented-out code in various configuration files
    - Delete unused plugin configuration files
    - Simplify keymaps and autocmds
    - Update lazy-lock.json with latest plugin versions
    ```
    
    The commit message follows the conventional commit format with a concise description, briefly explains the type of changes (chore), and provides short bullet points highlighting the key modifications across the configuration files.

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,7 +1,5 @@
 vim.g.color_light = "catppuccin-latte"
 vim.g.color_dark = "catppuccin-mocha"
--- vim.g.color_light = "everforest"
--- vim.g.color_dark = "everforest"
 
 return {
   { "roobert/tailwindcss-colorizer-cmp.nvim", config = true, lazy = true, enabled = false },
@@ -24,13 +22,11 @@ return {
     priority = 1000,
   },
   { 'shaunsingh/nord.nvim'},
-
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = vim.g.color_light,
       colorscheme = vim.g.color_dark,
-      -- colorscheme = "evangelion",
     },
   },
   {
@@ -40,20 +36,6 @@ return {
   },
   {
     "alexxGmZ/e-ink.nvim",
-    -- priority = 1000,
-    -- config = function ()
-    --  require("e-ink").setup()
-    --  vim.cmd.colorscheme "e-ink"
-
-    -- choose light mode or dark mode
-    -- -- vim.opt.background = "dark"
-    -- -- vim.opt.background = "light"
-    -- --
-    -- -- or do
-    -- -- :set background=dark
-    -- -- :set background=light
-    --  end
-    --  }
   },
   {
     "sainnhe/everforest",
--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -1,11 +1,2 @@
 return {
-  -- {
-  --   "akinsho/bufferline.nvim",
-  --   init = function()
-  --     local bufline = require("catppuccin.groups.integrations.bufferline")
-  --     function bufline.get()
-  --       return bufline.get_theme()
-  --     end
-  --   end,
-  -- },
 }
