69f0f2acc52e7e32b82c01c5716c100fcefb04e4
Jeff Buttars <jeffbuttars@gmail.com>
2025-10-17 08:43:26 -0600

    refactor: streamline NeovimTips keymaps
    - Replace `init` function with `keys` configuration
    - Simplify key bindings with more concise leader keys
    - Use Lua's native key mapping structure

--- lua/plugins/neovimtips.lua
+++ lua/plugins/neovimtips.lua
@@ -24,45 +24,14 @@ return {
     -- OPTIONAL: Bookmark symbol (default: "🌟 ")
     -- bookmark_symbol = "🌟 ",
   },
-  init = function()
-    -- OPTIONAL: Change to your liking or drop completely
+  keys = {
     -- The plugin does not provide default key mappings, only commands
-    local map = vim.keymap.set
-    map(
-      "n",
-      "<leader>not",
-      ":NeovimTips<CR>",
-      { desc = "Neovim tips", noremap = true, silent = true }
-    )
-    map(
-      "n",
-      "<leader>nte",
-      ":NeovimTipsEdit<CR>",
-      { desc = "Edit your Neovim tips", noremap = true, silent = true }
-    )
-    map(
-      "n",
-      "<leader>nta",
-      ":NeovimTipsAdd<CR>",
-      { desc = "Add your Neovim tip", noremap = true, silent = true }
-    )
-    map(
-      "n",
-      "<leader>nth",
-      ":help neovim-tips<CR>",
-      { desc = "Neovim tips help", noremap = true, silent = true }
-    )
-    map(
-      "n",
-      "<leader>ntr",
-      ":NeovimTipsRandom<CR>",
-      { desc = "Show random tip", noremap = true, silent = true }
-    )
-    map(
-      "n",
-      "<leader>ntp",
-      ":NeovimTipsPdf<CR>",
-      { desc = "Open Neovim tips PDF", noremap = true, silent = true }
-    )
-  end,
+    { "<leader>N", ":NeovimTips", desc = "Neovim tips", mode = { "n" } },
+    { "<leader>No", ":NeovimTips<CR>", desc = "Open Tips", mode = { "n" } },
+    { "<leader>Ne", ":NeovimTipsEdit<CR>", desc = "Edit your Neovim tips", mode = { "n" } },
+    { "<leader>Na", ":NeovimTipsAdd<CR>", desc = "Add your Neovim tip", mode = { "n" } },
+    { "<leader>Nh", ":help neovim-tips<CR>", desc = "Neovim tips help", mode = { "n" } },
+    { "<leader>Nr", ":NeovimTipsRandom<CR>", desc = "Show random tip", mode = { "n" } },
+    { "<leader>Np", ":NeovimTipsPdf<CR>", desc = "Open Neovim tips PDF", mode = { "n" } },
+  },
 }
