c017d3d84d113fbff540af3cb3e042d7e05f7bb9
Jeff Buttars <jeffbuttars@gmail.com>
2025-10-30 07:32:34 -0600

    chore: update lazy-lock and add emoji completion
    - Add blink-emoji.nvim plugin
    - Update fzf-lua, neo-tree.nvim, nvim-lspconfig, nvim-treesitter, snacks.nvim, todo-comments.nvim, tokyonight.nvim
    - Configure emoji source for blink.cmp with colon trigger

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -3,7 +3,40 @@ local blink_cmp = require("blink.cmp")
 return {
   {
     "saghen/blink.cmp",
+
+    dependencies = {
+      "moyiz/blink-emoji.nvim",
+    },
+
     opts = {
+      sources = {
+        default = {
+          "emoji",
+        },
+        providers = {
+          emoji = {
+            module = "blink-emoji",
+            name = "Emoji",
+            score_offset = 15, -- Tune by preference
+            opts = {
+              insert = true, -- Insert emoji (default) or complete its name
+              ---@type string|table|fun():table
+              trigger = function()
+                return { ":" }
+              end,
+            },
+            -- should_show_items = function()
+            --   return vim.tbl_contains(
+            --     -- Enable emoji completion only for git commits and markdown.
+            --     -- By default, enabled for all file-types.
+            --     { "gitcommit", "markdown" },
+            --     vim.o.filetype
+            --   )
+            -- end,
+          },
+        },
+      },
+
       completion = {
         list = {
           -- Don't select by default and only use an explicitly selected choice
