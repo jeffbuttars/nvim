185b50e4ba508445d736527d3d8c153ed48f973b
Jeff Buttars <jeffbuttars@gmail.com>
2025-11-01 10:12:38 -0600

    feat: configure tokyonight transparency and snacks dashboard - Enable transparent background in tokyonight with sidebars and floats - Add snacks dashboard with git-related sections and GitHub integration - Uncomment Snacks require statement - Remove unused explorer config from snacks

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -9,9 +9,15 @@ return {
   { "rose-pine/neovim", name = "rose-pine" },
   {
     "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
+    -- lazy = false,
+    -- priority = 1000,
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
 
   { "rebelot/kanagawa.nvim" },
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,6 +1,4 @@
--- local Snacks = require("snacks")
--- Setup tagfunc to work with lsp for ctags style tag based navigation with goto definition
--- vim.opt.tagfunc = Snacks.picker.lsp_definitions
+local Snacks = require("snacks")
 
 return {
   {
@@ -10,9 +8,6 @@ return {
         configure = true,
         theme_path = vim.fs.normalize("~/.config/lazygit/catpuccin/themes/macchiato/rosewater.yml"),
       },
-      --   explorer = {
-      --     jump = { close = true },
-      --   },
       --     animate = {
       --       enabled = true,
       --       -- ---@class snacks.animate.Config
@@ -30,6 +25,92 @@ return {
       --     zen = {
       --       width = 120,
       --     },
+      dashboard = {
+        preset = {
+          header = [[
+
+          ]],
+        },
+
+        sections = {
+          { section = "header" },
+          {
+            pane = 2,
+            section = "header",
+          },
+          -- {
+          --   pane = 2,
+          --   section = "terminal",
+          --   cmd = "colorscript -e square",
+          --   height = 5,
+          --   padding = 1,
+          -- },
+          { section = "keys", gap = 1, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            desc = "Browse Repo",
+            padding = 1,
+            key = "b",
+            action = function()
+              Snacks.gitbrowse()
+            end,
+          },
+          function()
+            local in_git = Snacks.git.get_root() ~= nil
+            local cmds = {
+              {
+                title = "Notifications",
+                cmd = "gh notify -s -a -n5",
+                action = function()
+                  vim.ui.open("https://github.com/notifications")
+                end,
+                key = "n",
+                icon = " ",
+                height = 5,
+                enabled = true,
+              },
+              {
+                title = "Open Issues",
+                cmd = "gh issue list -L 3",
+                key = "i",
+                action = function()
+                  vim.fn.jobstart("gh issue list --web", { detach = true })
+                end,
+                icon = " ",
+                height = 7,
+              },
+              {
+                icon = " ",
+                title = "Open PRs",
+                cmd = "gh pr list -L 3",
+                key = "P",
+                action = function()
+                  vim.fn.jobstart("gh pr list --web", { detach = true })
+                end,
+                height = 7,
+              },
+              {
+                icon = " ",
+                title = "Git Status",
+                cmd = "git --no-pager diff --stat -B -M -C",
+                height = 10,
+              },
+            }
+            return vim.tbl_map(function(cmd)
+              return vim.tbl_extend("force", {
+                pane = 2,
+                section = "terminal",
+                enabled = in_git,
+                padding = 1,
+                ttl = 5 * 60,
+                indent = 3,
+              }, cmd)
+            end, cmds)
+          end,
+          { section = "startup" },
+        },
+      },
     },
   },
 }
