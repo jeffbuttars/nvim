f2835b715adcf1e05b497e3a11b73041df7104ba
Jeff Buttars <jeffbuttars@gmail.com>
2026-02-08 12:33:57 -0700

    feat: add DoubleTap plugin configuration - New plugin spec for Codeberg DoubleTap repository - Include commented options for local development and debug mode

---
+++ lua/plugins/doubletap.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "https://codeberg.org/pr0x/DoubleTap",
+
+    -- This tells lazy.nvim it's a local plugin, not from GitHub
+    -- name = "DoubleTap",
+    -- dir = "~/Dev/DoubleTap/",
+    enabled = true,
+
+    -- opts = {
+    --   debug = true,
+    -- },
+  },
+}
