6be4ff5f339213ea40c63e0e9312ffb9b1fac1a4
Jeff Buttars <jeffbuttars@gmail.com>
2026-02-09 09:27:09 -0700

    fix: resolve merge conflict in doubletap plugin config - Remove duplicate config entries - Keep commented-out local plugin setup

--- lua/plugins/doubletap.lua
+++ lua/plugins/doubletap.lua
@@ -3,17 +3,9 @@ return {
     "https://codeberg.org/pr0x/DoubleTap",
 
     -- This tells lazy.nvim it's a local plugin, not from GitHub
-<<<<<<< HEAD
     -- name = "DoubleTap",
     -- dir = "~/Dev/DoubleTap/",
 
-=======
-    name = "DoubleTap",
-    dir = "~/Dev/DoubleTap/",
-
-    enabled = true,
-
->>>>>>> 1bfef44cb69a1bd2ad1631180af97c59f76aad88
     -- opts = {
     --   debug = true,
     -- },
