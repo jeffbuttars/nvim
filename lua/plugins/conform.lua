43b3faefc34b6ac4ae5002be65a58d9ae53651f0
Jeff Buttars <jeffbuttars@gmail.com>
2026-03-24 11:47:07 -0600

    chore: update dependencies and formatter config - Remove blink-copilot, add octo.nvim - Enable biome, black, prettier formatting extras - Add octo util extra - Simplify javascript formatter to prettier only - Add typescript and svelte prettier formatting - Remove eslint-lsp, standardjs from mason - Add svelte-language-server, typescript-language-server, tailwindcss-language-server

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,17 +14,17 @@ return {
         },
       },
       formatters_by_ft = {
-        -- python = { "ruff_fix", "black", "isort" },
         -- ["htmldjango"] = { "djlint" },
-        -- For some reason this version creates an empty file
-        -- python = { "ruff_fix", "fuff_format", "black" },
         -- Use a sub-list to run only the first available formatter
-        javascript = { "standardjs", "prettierd", "prettier", stop_after_first = true },
-        -- css = { "stylelint" },
+        -- javascript = { "standardjs", "prettierd", "prettier", stop_after_first = true },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        svelte = { "prettier" },
+        -- css = { "stylelint", stop_after_first = true },
         -- bash = { "shfmt", "beautysh" },
         -- sh = { "shfmt", "beautysh" },
-        -- json = { "jq" },
-        -- yaml = { "yamlfix" },
+        -- json = { "jq", "prettier", stop_after_first = true},
+        -- yaml = { "yamlfix", stop_after_first = true },
         -- c = { "clang_format" },
         go = { "golines", "goimports", "gofumpt", "gofmt", "pruner" },
         -- Use the "*" filetype to run formatters on all filetypes.
