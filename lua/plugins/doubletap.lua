de1a1f9f56c744f9b1cf3cf3daa600a5e8666cb3
Jeff Buttars <jeffbuttars@gmail.com>
2026-04-07 08:39:38 -0600

    chore: update plugin dependencies and refactor keymaps - Update plugin versions via lazy-lock.json - Move blink.cmp require after keymaps are deleted - Add restart keymaps for session management - Enable walkout mode in DoubleTap plugin

--- lua/plugins/doubletap.lua
+++ lua/plugins/doubletap.lua
@@ -2,12 +2,12 @@ return {
   {
     "https://codeberg.org/pr0x/DoubleTap.git",
     -- This tells lazy.nvim it's a local plugin, not from GitHub
-    -- name = "DoubleTap",
-    -- dir = "~/Dev/DoubleTap/",
+    name = "DoubleTap",
+    dir = "~/Dev/DoubleTap/",
 
     opts = {
       --   debug = true,
-      walkout = false,
+      walkout = true,
     },
   },
 }
