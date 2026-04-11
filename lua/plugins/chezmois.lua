9e44a09dff294b816641b9e041a26d7085e215cc
Jeff Buttars <jeffbuttars@gmail.com>
2026-04-11 11:11:47 -0600

    feat: improve session restart and add chezmoi plugin - Refactor session restart keymap with better notification and file reload - Add chezmoi.nvim plugin for dotfile management

---
+++ lua/plugins/chezmois.lua
@@ -0,0 +1,10 @@
+-- Lazy.nvim
+return {
+  "xvzc/chezmoi.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("chezmoi").setup({
+      -- your configurations
+    })
+  end,
+}
